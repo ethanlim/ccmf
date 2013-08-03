50079e3edc43ca616dfec0fe283dc0968208e224
ethanlim <mail@ethanlim.net>
2013-08-03 17:38:23 +0800

    Remove useless comments

--- index.js
+++ index.js
@@ -8,6 +8,4 @@ var fs = require('fs');
 filedata = fs.readFileSync(__dirname+'/lib/build/ccmf.js','utf8');
 eval(filedata);
 
-/* The quadtree.js file defines a class 'QuadTree' which is all we want to export */
-
 exports.ccmf = ccmf;
