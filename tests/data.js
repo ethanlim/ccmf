5f25a28c67f6452a3e51a878debad9e642b0902e
ethanlim <mail@ethanlim.net>
2013-08-10 14:28:57 +0800

    Added the text and data test files

---
+++ tests/data.js
@@ -0,0 +1,83 @@
+var ccmf = require('ccmf'),
+	fs = require('fs'),
+	winston = require('winston');
+
+/**
+ * 	Testing Parameters
+ */
+var n = 1,
+	testFileName = '/tests/data/data-performance.js',
+	sampleFile = '../../samples/reuters/reut2-000.sgm',
+	outputFileName = '../../logs/tests/data-storelsh.txt',
+	startTime = null,
+	elapsedTime = null,
+	recvAck = 0;
+
+/**
+ *  Logger
+ */
+winston.profile('test');
+
+var logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)(),
+      new (winston.transports.File)({filename: outputFileName })
+    ]
+});
+
+module.exports = {
+		 setUp: function (callback) {
+				fs.exists(outputFileName, function(exists) {
+					  if (exists) {
+						  fs.unlink(outputFileName);
+					  } 
+				});	
+		        callback();
+		 },
+		 store:function (test){
+			 fs.readFile(sampleFile,function read(err,data){
+					if(err){
+						console.log('Error Reading File : '+err);
+					}
+					else{
+						var content = data,
+						textContent = content.toString(),
+						registeringText = '',
+						bodyIdx = 0,
+						max = 0;
+					
+						bodyTextArr  = textContent.match(/<\s*BODY[^>]*>([^<]*)<\s*\/\s*BODY\s*>/g);
+						
+						if(bodyTextArr!==null){
+							
+							max=bodyTextArr.length;
+							if(n<max){
+								max = n;
+							}
+							
+							for(bodyIdx=0;bodyIdx<max;bodyIdx++){
+								
+								registeringText = bodyTextArr[bodyIdx].replace(/(<([^>]+)>)/ig,"");
+								
+								var textMod = ccmf.ccmf.Text.create();
+								var dataMod = ccmf.ccmf.Data.create();
+								
+								var registeringTextShingles = textMod.removedStopWordShingles(registeringText,9);
+								
+								var registerShinglesFing = textMod.shinglesFingerprintConv(registeringTextShingles);
+								
+								var minHashSignatures = textMod.minHashSignaturesGen(registerShinglesFing);
+								
+								/* Register the Signature into the Registry*/
+								startTime = process.hrtime();
+								
+								dataMod.storeLsh(minHashSignatures[0],testAsync);
+							}
+						}else{
+							console.log("No String Found");
+						}
+					}
+				});
+			 	test.done();
+		 }
+};
---
+++ tests/text.js
@@ -0,0 +1,293 @@
+var ccmf = require('ccmf'),
+	fs = require('fs'),
+	winston = require('winston');
+
+/**
+ * 	Testing Parameters
+ */
+var n = 1000,
+	startTime = null,
+	elapsedTime = null,
+	testFileName = '/tests/text.js' ;
+
+/**
+ *  Logger
+ */
+winston.profile('test');
+
+module.exports.shingles = {
+		 setUp: function (callback) {
+				this.sampleFile = '../samples/reuters/reut2-000.sgm';
+			 	outputFileName = '../logs/tests/shingles.log';
+				
+			    this.logger = new (winston.Logger)({
+			        transports: [
+			                  	new (winston.transports.Console)(),
+			                  	new (winston.transports.File)({filename: outputFileName })
+			                  ]
+			    });
+			    
+				fs.exists(outputFileName, function(exists) {
+					  if (exists) {
+						  fs.unlink(outputFileName);
+					  } 
+				});	
+				
+		        callback();
+		 },
+		 tearDown: function (callback) {
+		        // clean up
+		        callback();
+		 },
+		 extractionSpeed: function (test) {
+			 
+			 /* Remove the console logger */
+			 var logger = this.logger;
+			 logger.remove(winston.transports.Console);
+			 			 
+			 fs.readFile(this.sampleFile,function read(err,data){
+					if(err){
+						console.log('Error Reading File : '+err);
+					}
+					else{
+						var content = data,
+						textContent = content.toString(),
+						registeringText = '',
+						bodyIdx = 0,
+						numOfArticles = 0,
+						textMod = null,
+						articleWordCount = 0;
+					
+						bodyTextArr  = textContent.match(/<\s*BODY[^>]*>([^<]*)<\s*\/\s*BODY\s*>/g);
+						
+						if(bodyTextArr!==null){
+							
+							numOfArticles=bodyTextArr.length;
+							
+							for(bodyIdx=0;bodyIdx<numOfArticles;bodyIdx++){
+								
+								registeringText = bodyTextArr[bodyIdx].replace(/(<([^>]+)>)/ig,"");
+								
+								textMod = ccmf.ccmf.Text.create();
+								
+								articleWordCount = registeringText.split('').length;
+								
+								startTime = process.hrtime();
+								
+								textMod.removedStopWordShingles(registeringText,9);				
+														
+								elapsedTime = process.hrtime(startTime);
+									
+								logger.log('info',
+										{
+											testFile:testFileName,
+											purpose:'shingles-extraction-speed',
+											description:'Shingles Extraction Speed',
+											textId:bodyIdx,
+											connectionType:'none',
+											elapsedTime:elapsedTime[1],
+											timeType:'ns',
+											textLen:articleWordCount
+										}
+								);
+							}
+							console.log("Number of Articles : "+numOfArticles);
+						}else{
+							console.log("No String Found");
+						}
+					}
+				});
+		        test.done();
+		 }
+};
+
+module.exports.minhash = {
+		 setUp: function (callback) {
+			this.sampleFile = '../samples/reuters/reut2-000.sgm';
+		 	outputFileName = '../logs/tests/minhash.log';
+			
+		    this.logger = new (winston.Logger)({
+		        transports: [
+		                  	new (winston.transports.Console)(),
+		                  	new (winston.transports.File)({filename:outputFileName})
+		                  ]
+		    });
+		    
+			fs.exists(outputFileName, function(exists) {
+				  if (exists) {
+					  fs.unlink(outputFileName);
+				  } 
+			});	
+			
+	        callback();
+		 },
+		 tearDown: function (callback) {
+		        // clean up
+			 callback();
+		 },
+		 constructingSpeed: function (test) {
+			 
+			 /* Remove the console logger */
+			 var logger = this.logger;
+			 logger.remove(winston.transports.Console);
+			 
+			 fs.readFile(this.sampleFile,function read(err,data){
+					if(err){
+						console.log('Error Reading File : '+err);
+					}
+					else{
+						var content = data,
+						textContent = content.toString(),
+						registeringText = '',
+						bodyIdx = 0,
+						numOfArticles = 0,
+						bodyTextArr = textContent.match(/<\s*BODY[^>]*>([^<]*)<\s*\/\s*BODY\s*>/g),
+						textMod = null,
+						articleNo = 0,
+						articleWordCount = 0,
+						registeringTextShingles = null,
+						registerShinglesFing = null;
+						
+						if(bodyTextArr!==null){
+							
+							numOfArticles=bodyTextArr.length;
+								
+							for(bodyIdx=0;bodyIdx<numOfArticles;bodyIdx++){
+								
+								registeringText = bodyTextArr[bodyIdx].replace(/(<([^>]+)>)/ig,"");
+								
+								textMod = ccmf.ccmf.Text.create();
+								
+								articleNo = Math.floor((Math.random()*(numOfArticles-1))+0);
+								
+								articleWordCount = registeringText.split('').length;
+									
+								registeringTextShingles = textMod.removedStopWordShingles(registeringText,9);	
+								
+								registerShinglesFing = textMod.shinglesFingerprintConv(registeringTextShingles);
+								
+								startTime = process.hrtime();
+								
+								textMod.minHashSignaturesGen(registerShinglesFing);
+														
+								elapsedTime = process.hrtime(startTime);
+									
+								logger.log('info',
+										{
+											testFile:testFileName,
+											purpose:'minhash-signature',
+											description:'Minhash Signature Generation',
+											textId:articleNo,
+											connectionType:'none',
+											elapsedTime:elapsedTime[1],
+											timeType:'ns',
+											textLen:articleWordCount
+										}
+								);
+								
+							}
+							console.log("Number of Articles : "+numOfArticles);
+						}else{
+							console.log("No String Found");
+						}
+					}
+				});
+			 	test.done();
+		 }
+};
+
+module.exports.lsh = {
+		setUp: function (callback) {
+			this.sampleFile = '../samples/reuters/reut2-000.sgm';
+		 	outputFileName = '../logs/tests/lsh.log';
+			
+		    this.logger = new (winston.Logger)({
+		        transports: [
+		                  	new (winston.transports.Console)(),
+		                  	new (winston.transports.File)({filename:outputFileName})
+		                  ]
+		    });
+		    
+			fs.exists(outputFileName, function(exists) {
+				  if (exists) {
+					  fs.unlink(outputFileName);
+				  } 
+			});	
+			
+	        callback();
+		 },
+		 tearDown: function (callback) {
+		     // clean up
+			 callback();
+		 },
+		 constructingSpeed: function (test){
+			 
+			 var logger = this.logger;
+			 
+			 fs.readFile(this.sampleFile,function read(err,data){
+					if(err){
+						console.log('Error Reading File : '+err);
+					}
+					else{
+						var content = data,
+						textContent = content.toString(),
+						registeringText = '',
+						bodyIdx = 0,
+						numOfArticles = 0,
+						textMod = null,
+						articleNo = 0,
+						articleWordCount = 0,
+						registeringTextShingles = null,
+						registerShinglesFing = null
+						signatures = null;
+					
+						bodyTextArr  = textContent.match(/<\s*BODY[^>]*>([^<]*)<\s*\/\s*BODY\s*>/g);
+						
+						if(bodyTextArr!==null){
+							
+							numOfArticles = bodyTextArr.length;
+						
+							for(bodyIdx=0;bodyIdx<numOfArticles;bodyIdx++){
+								
+								registeringText = bodyTextArr[bodyIdx].replace(/(<([^>]+)>)/ig,"");
+								
+								textMod = ccmf.ccmf.Text.create();
+								
+								articleNo = Math.floor((Math.random()*(numOfArticles-1))+0);
+								
+								articleWordCount = registeringText.split('').length;
+									
+								registeringTextShingles = textMod.removedStopWordShingles(registeringText,9);	
+								
+								registerShinglesFing = textMod.shinglesFingerprintConv(registeringTextShingles);
+								
+								signatures = textMod.minHashSignaturesGen(registerShinglesFing);
+								
+								startTime = process.hrtime();
+								
+								textMod.LSH(signatures[0]);
+														
+								elapsedTime = process.hrtime(startTime);
+									
+								logger.log('info',
+										{
+											testFile:testFileName,
+											purpose:'lsh-time',
+											description:'Speed of LSH execution',
+											textId:articleNo,
+											connectionType:'none',
+											elapsedTime:elapsedTime[1],
+											timeType:'ns',
+											textLen:articleWordCount
+										}
+								);  
+							}
+							console.log("Number of Articles : "+numOfArticles);
+						}else{
+							console.log("No String Found");
+						}
+					}
+				});
+			 test.done();
+		 }
+};
