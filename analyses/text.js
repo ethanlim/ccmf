b5eb74def4ea2c0e2f19255dffac09233c05b869
ethanlim <mail@ethanlim.net>
2013-08-12 19:19:56 +0800

    Initial Commit of Analyzer

---
+++ analyses/text.js
@@ -0,0 +1,44 @@
+var ccmf = require('ccmf'),
+	fs = require('fs'),
+	logFiles = new Array(),
+	readline = require('readline'),
+	analyzer = new Analyzer(),
+	pathToLogFiles = '../logs/tests/';
+
+
+var logFilesName = fs.readdir(pathToLogFiles,analyzer.processFiles);
+
+function Analyzer(){
+	
+	this.processFiles = function(err,files){
+		
+		var indexOfEmptyFile = files.indexOf('empty');
+		
+		files.splice(indexOfEmptyFile,1);
+		
+		for(logFileNum in files){
+			
+			fs.readFile(pathToLogFiles+files[logFileNum], function(err, data) {
+			    if(err) throw err;
+			    
+			    var textArray = data.toString().split("\n"),
+			    numOfText = textArray.length,
+			    jsonEntry = null,
+			    totalTimeTaken = 0;
+			    
+			    for(line in textArray) {
+			    	if(textArray[line]!==""){
+				        jsonEntry = JSON.parse(textArray[line]);
+				        totalTimeTaken += jsonEntry.elapsedTime;
+			    	}
+			    }
+			    
+			    console.log('Analysis Type : '+jsonEntry.purpose);
+			    console.log('Num of Text : '+numOfText);
+			    console.log('Total Time : '+totalTimeTaken);
+			    console.log('Mean Time :'+(totalTimeTaken/numOfText)/1000000000);
+			});
+		}
+	};
+};
+
